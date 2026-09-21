d7f35772356a6b554c89000e9a7041f4a75a4399
zukasdovis <zukasdovis@gmail.com>
2026-09-21 23:29:45 +0300

    commit

--- main.cpp
+++ main.cpp
@@ -15,7 +15,6 @@ int hexToInt(char c)
 
     return -1;
 }
-
 int countSetBits(int x)
 {
     int count = 0;
@@ -28,7 +27,6 @@ int countSetBits(int x)
 
     return count;
 }
-
 double compare(string a, string b)
 {
     if (a.length() != b.length())
@@ -60,6 +58,41 @@ double compare(string a, string b)
     return double(difBits) / totalBits;
 }
 
+uint8_t bitSplit(uint8_t a, uint8_t b){
+    uint8_t aHigh = a >> 4;
+    uint8_t aLow = a & 0x0F;
+
+    uint8_t bHigh = b >> 4;
+    uint8_t bLow = b & 0x0F;
+
+    uint8_t high = (aHigh + bHigh) & 0x0F;
+    uint8_t low = (aLow + bLow) & 0x0F;
+
+    return (high << 4) | low;
+}
+
+string hashFunction(const string& input){
+    
+    uint32_t state =0x12345678; 
+
+    for(size_t i=0; i < input.size(); i++){
+        uint8_t current = static_cast<uint8_t>(input[i]);
+
+        uint8_t next;
+
+        if(i+1<input.size())
+            next = static_cast<uint8_t>(input[i+1]);
+        else next = 0;
+
+        uint8_t key = bitSplit(current, next);
+
+        state ^= key;
+
+        state ^= key * 0x5D6FEBB8;
+    }
+
+}
+
 
 int main(){
 
@@ -75,11 +108,10 @@ int main(){
     }else if(x==1){
         getline(cin,input);
     }
-    while(compare()){
 
+    string hash=input;
 
-    }
-
+    
 
     return 0;
 }
