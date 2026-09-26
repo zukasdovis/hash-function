aec2569192a6beff2b2eb6370a67afb499fcc8b3
zukasdovis <zukasdovis@gmail.com>
2026-09-26 12:49:52 +0300

    su DI pagerinta hash funkcija

--- hash.cpp
+++ hash.cpp
@@ -1,6 +1,5 @@
 #include "hash.h"
 
-// Algortimas
 uint8_t bitSplit(uint8_t a, uint8_t b){
     uint8_t aHigh = a >> 4;
     uint8_t aLow = a & 0x0F;
@@ -16,33 +15,31 @@ uint8_t bitSplit(uint8_t a, uint8_t b){
 uint32_t rotateLeft(uint32_t x, int bits){
     return (x<<bits) | (x>>(32-bits));
 }
-uint32_t hashFunction(const string& input){
+
+uint32_t hashFunction(const std::string& input) {
+    uint32_t state = 0x9E3779B9;
     
-    uint32_t state =0x12345678; 
-
-    for(size_t i=0; i < input.size(); i++){
+    state ^= (uint32_t)input.size() * 0x85EBCA6B;
+    
+    for(size_t i = 0; i < input.size(); i++) {
         uint8_t current = static_cast<uint8_t>(input[i]);
-
-        uint8_t next;
-
-        if(i+1<input.size())
-            next = static_cast<uint8_t>(input[i+1]);
-        else next = 0;
-
+        uint8_t next = (i + 1 < input.size()) ? static_cast<uint8_t>(input[i+1]) : 0xFF;
         uint8_t key = bitSplit(current, next);
-
+        
+        state ^= ((current << (i % 4)) | (current >> (8 - i % 4)));
         state ^= key;
-
+        
         state += key * 0x5D6FEBB8;
-
-        state = rotateLeft(state,7);
+        state = rotateLeft(state, (7 + i) % 32);
     }
-
-    state ^= state >> 16;  
+    
+    state ^= state >> 15;
     state *= 0x57EFBCA6;
-
-    state ^= state >> 11;
+    state ^= state >> 13;
     state *= 0xCBED548A;
-
+    state ^= state >> 11;
+    state *= 0x9E3779B9;
+    state ^= state >> 16;
+    
     return state;
 }
